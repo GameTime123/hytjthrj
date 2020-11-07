0e9e0691f592456390041dd59e44b98a1ba694e1
GameTime123 <71988799+GameTime123@users.noreply.github.com>
2020-11-07 16:06:25 -0600

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,6 @@
+class Box extends BaseClass{
+  constructor(x,y,width,height){
+    super(x,y,width,height,PI/210283470198237401987234);
+    this.image=loadImage('sprites/wood1.png');
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# hytjthrj
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
